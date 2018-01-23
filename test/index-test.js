38a91120f7114cc5aacbd71d28b6203c99246d19
Ethel Cofresi <cofresi.ethel@gmail.com>
2018-01-23 02:44:36 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -23,5 +23,5 @@ describe('functions', () => {
     it('returns "It\'s ${days} days until ${holiday}!"', () => {
       expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
     })
-  })
-}) */
+  })  */
+}) 
