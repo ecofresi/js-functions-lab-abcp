b8cf1878332f264f33b09a17bdcb74a4b3df8b9a
Ethel Cofresi <cofresi.ethel@gmail.com>
2018-01-23 03:00:40 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -12,7 +12,7 @@ describe('functions', () => {
       expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
     })
   })
-/*
+
   describe('happyHolidayTo(holiday, name)', () => {
     it('returns "Happy ${holiday}, ${name}!"', () => {
       expect(happyHolidayTo('Independence Day', 'you')).toEqual('Happy Independence Day, you!')
@@ -23,5 +23,5 @@ describe('functions', () => {
     it('returns "It\'s ${days} days until ${holiday}!"', () => {
       expect(holidayCountdown("Mother's Day", 20)).toEqual("It's 20 days until Mother's Day!")
     })
-  })  */
+  })
 }) 
